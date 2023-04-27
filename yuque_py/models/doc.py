ce156892c25af4707a8664880117935cf85aca7e
loose <ustcghc@outlook.com>
2023-04-27 14:59:01 +0800

    add some func
    Signed-off-by: hcgu <hcgu@mail.ustc.edu.cn>

--- yuque_py/models/doc.py
+++ yuque_py/models/doc.py
@@ -6,6 +6,13 @@ from yuque_py.clients.abstract_client import AbstractClient
 class Doc:
     def __init__(self, client: AbstractClient):
         self._client = client
+    
+    def list(
+        self, namespace: str, data: typing.Optional[typing.Dict] = None
+    ) -> typing.Dict:
+        assert namespace
+        api = f"repos/{namespace}/docs"
+        return self._client.request(api,method='GET',requests_data=data)
 
     def get(
         self, namespace: str, slug: str, data: typing.Optional[typing.Dict] = None
